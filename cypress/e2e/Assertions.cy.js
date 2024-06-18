ee8b4ea4fcccdad2f62b27749b290cdf41d8f826
Himanshu Gilhare <himanshu.gilhare@tartanhq.com>
2024-06-18 17:54:41 +0530

    assertions

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -1,7 +1,10 @@
+
+
 describe('Cypress Assertions',()=>{
     it('implict assertions',()=>{
 
         /// should or and
+        cy.viewport(1280,720)
 
         cy.visit("https://www.saucedemo.com/")
         cy.url().should('include','saucedemo')
@@ -11,9 +14,27 @@ describe('Cypress Assertions',()=>{
         cy.url().should('include','saucedemo')
         .should('contain','www.saucedemo')
         .and('eq','https://www.saucedemo.com/')
-        .and('not.eq','kjhgfghj')
+        .and('not.eq','kjhgfghj')  //// negative assertions
 
+        cy.get('.login_logo').should('be.visible') /// is displayed
+        cy.get('.login_logo').should('exist') 
+    })
+    it('explicit assertions',()=>{
+        cy.viewport(1280,720)
+        cy.visit("https://hrmssync.tartanhq.com/")
+        cy.get('#username').type("Himanshu.gilhare@tartanhq.com")
+        cy.get('#password').type("Himanshu@08")
+        cy.get('.chakra-button').click()
+        let expectedname= "👋 Welcome Himanshu!";
+        cy.get('.text-grey-800').then((x)=>{
+            let actualname=x.text()
+            //BDD
+            expect(expectedname).to.equal(actualname)
+            ///for not equal --- to.equal(actualname)
 
+            //TDD
+            assert.equal(actualname,expectedname)
+        })
     })
 
 })
